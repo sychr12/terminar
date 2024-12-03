a742b6cff036916d0422be9e43748dfd6127e62d
Luiz Felipe <saychr8@gmail.com>
2024-12-03 12:52:24 -0400

    tenho que terminar ainda hoje

---
+++ Intranet atual/script.js
@@ -0,0 +1,11 @@
+// Função para abrir a sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "280px"; 
+    document.getElementById("main").style.marginRight = "280px"; 
+}
+
+// Função para fechar a sidebar
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0"; 
+    document.getElementById("main").style.marginRight = "0"; 
+}
