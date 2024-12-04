aa932c22264c618706aa97ba98a8b1b9fa538659
Luiz Felipe <saychr8@gmail.com>
2024-12-04 13:32:07 -0400

    terminar com urgência

---
+++ Intranet atual/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "400px"; 
+    document.getElementById("main").style.marginRight = "370px"; 
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0"; 
+    document.getElementById("main").style.marginRight = "0"; 
+}
