464b824137748f04d33144ccb5a05b5ecf9f3a85
Luiz Felipe <saychr8@gmail.com>
2024-12-05 12:59:47 -0400

    Add files via upload

---
+++ Intranet atual/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "400px";  
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0"; 
+}
+
