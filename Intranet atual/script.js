40775811defb2281ff731a2adc37ab1ed5f6d330
Luiz Felipe <saychr8@gmail.com>
2024-12-05 12:59:21 -0400

    Delete Intranet atual directory

--- Intranet atual/script.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "400px"; 
-    document.getElementById("main").style.marginRight = "370px"; 
-}
-
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0"; 
-    document.getElementById("main").style.marginRight = "0"; 
-}
